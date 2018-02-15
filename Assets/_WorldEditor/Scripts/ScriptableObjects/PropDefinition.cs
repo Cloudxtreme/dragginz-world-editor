a39372723f7cfc1403e690e7e1b1e5c6cee2bee4
obrodhage <obrodhage@gmail.com>
2018-02-15 14:57:03 -0800

    added scriptable objects for prop definitions

---
+++ Assets/_WorldEditor/Scripts/ScriptableObjects/PropDefinition.cs
@@ -0,0 +1,18 @@
+﻿//
+// Author  : Oliver Brodhage
+// Company : Decentralised Team of Developers
+//
+
+using UnityEngine;
+using System.Collections;
+
+namespace DragginzWorldEditor
+{
+	[CreateAssetMenu(fileName = "NewProp", menuName = "Dragginz/Prop", order = 1)]
+	public class PropDefinition : ScriptableObject
+	{
+		public string propName     = "New Prop";
+		public GameObject prefab   = null;
+		public bool isUsingGravity = true;
+	}
+}
