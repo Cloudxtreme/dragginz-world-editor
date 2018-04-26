a880a6f30fadb09aeb1ced7e462a1180f2d48b1b
obrodhage <obrodhage@gmail.com>
2018-04-26 14:24:50 -0700

    mesh uv fun

--- Assets/__VoxelWorldEditor/Scripts/Utils/MarchingCubes/ConvertLevelToMesh.cs
+++ Assets/__VoxelWorldEditor/Scripts/Utils/MarchingCubes/ConvertLevelToMesh.cs
@@ -16,8 +16,6 @@ namespace DragginzVoxelWorldEditor
 
 		public VoxelUtils.MARCHING_MODE mode = VoxelUtils.MARCHING_MODE.CUBES;
 
-        //private int seed = 0;
-
 		private int width, height, depth;
 		private float[] voxels;
 		private Vector3 pos;
@@ -89,7 +87,6 @@ namespace DragginzVoxelWorldEditor
             // Need to split the verts between multiple meshes.
             int maxVertsPerMesh = 30000; //must be divisible by 3, ie 3 verts == 1 triangle
 			int numMeshes = numVerts / maxVertsPerMesh + 1;
-			Debug.Log ("numVerts: "+numVerts);
 
 			List<Vector3> splitVerts = new List<Vector3>();
 			List<int> splitIndices = new List<int>();
@@ -117,21 +114,23 @@ namespace DragginzVoxelWorldEditor
 
                 if (splitVerts.Count == 0) continue;
 
-				//Debug.Log ("verts: " + splitVerts.Count);
-				//Debug.Log ("indices: " + splitIndices.Count);
-
                 Mesh mesh = new Mesh();
                 mesh.SetVertices(splitVerts);
                 mesh.SetTriangles(splitIndices, 0);
 				//mesh.SetUVs (0, splitUVs);
+
+				//Debug.Log ("verts: " + splitVerts.Count);
+				AutoWeld (mesh, 0.001f, 1f);
+				//Debug.Log ("mesh.vertices: " + mesh.vertices.Length);
+
                 mesh.RecalculateBounds();
-                mesh.RecalculateNormals();
+                //mesh.RecalculateNormals();
 
                 GameObject go = new GameObject("Mesh");
                 go.transform.parent = transform;
                 filter = go.AddComponent<MeshFilter>();
                 renderer = go.AddComponent<MeshRenderer>();
-				renderer.material = LevelEditor.Instance.aMaterials [0];// m_material;
+				renderer.material = LevelEditor.Instance.aToolMaterials [0];
                 filter.mesh = mesh;
 				go.AddComponent<MeshCollider> (); //collider = 
 
@@ -145,5 +144,122 @@ namespace DragginzVoxelWorldEditor
             }
 			Debug.LogWarning ("Time to split mesh data: " + (Time.realtimeSinceStartup - timer).ToString ());
         }
-    }
+
+		//
+		private void AutoWeld (Mesh mesh, float threshold, float bucketStep)
+		{
+			int i, len;
+
+			Vector3[] oldVertices = mesh.vertices;
+			Vector3[] newVertices = new Vector3[oldVertices.Length];
+			int[] old2new = new int[oldVertices.Length];
+			int newSize = 0;
+
+			// Find AABB
+			Vector3 min = new Vector3 (float.MaxValue, float.MaxValue, float.MaxValue);
+			Vector3 max = new Vector3 (float.MinValue, float.MinValue, float.MinValue);
+			len = oldVertices.Length;
+			for (i = 0; i < len; i++) {
+				if (oldVertices[i].x < min.x) min.x = oldVertices[i].x;
+				if (oldVertices[i].y < min.y) min.y = oldVertices[i].y;
+				if (oldVertices[i].z < min.z) min.z = oldVertices[i].z;
+				if (oldVertices[i].x > max.x) max.x = oldVertices[i].x;
+				if (oldVertices[i].y > max.y) max.y = oldVertices[i].y;
+				if (oldVertices[i].z > max.z) max.z = oldVertices[i].z;
+			}
+
+			// Make cubic buckets, each with dimensions "bucketStep"
+			int bucketSizeX = Mathf.FloorToInt ((max.x - min.x) / bucketStep) + 1;
+			int bucketSizeY = Mathf.FloorToInt ((max.y - min.y) / bucketStep) + 1;
+			int bucketSizeZ = Mathf.FloorToInt ((max.z - min.z) / bucketStep) + 1;
+			List<int>[,,] buckets = new List<int>[bucketSizeX, bucketSizeY, bucketSizeZ];
+
+			// Make new vertices
+			len = oldVertices.Length;
+			for (i = 0; i < len; i++) {
+				// Determine which bucket it belongs to
+				int x = Mathf.FloorToInt ((oldVertices[i].x - min.x) / bucketStep);
+				int y = Mathf.FloorToInt ((oldVertices[i].y - min.y) / bucketStep);
+				int z = Mathf.FloorToInt ((oldVertices[i].z - min.z) / bucketStep);
+
+				// Check to see if it's already been added
+				if (buckets[x, y, z] == null) {
+					buckets[x, y, z] = new List<int> (); // Make buckets lazily
+				}
+
+				int j, len2 = buckets [x, y, z].Count;
+				for (j = 0; j < len2; j++) {
+					Vector3 to = newVertices[buckets[x, y, z][j]] - oldVertices[i];
+					if (Vector3.SqrMagnitude (to) < threshold) {
+						old2new[i] = buckets[x, y, z][j];
+						goto skip; // Skip to next old vertex if this one is already there
+					}
+				}
+
+				// Add new vertex
+				newVertices[newSize] = oldVertices[i];
+				buckets[x, y, z].Add (newSize);
+				old2new[i] = newSize;
+				newSize++;
+
+				skip:;
+			}
+
+			// Make new triangles
+			int[] oldTris = mesh.triangles;
+			int[] newTris = new int[oldTris.Length];
+			len = oldTris.Length;
+			for (i = 0; i < len; i++) {
+				newTris[i] = old2new[oldTris[i]];
+			}
+
+			Vector3[] finalVertices = new Vector3[newSize];
+			for (i = 0; i < newSize; i++) {
+				finalVertices [i] = newVertices [i];
+			}
+
+			mesh.Clear();
+			mesh.vertices = finalVertices;
+			mesh.triangles = newTris;
+
+			mesh.RecalculateNormals();
+
+			Vector3[] normals = mesh.normals;
+			Vector2[] finalUV = new Vector2[newSize];
+
+			Vector3 v1, v2, v3;
+			Vector3 side1, side2, perp;
+
+			len = newTris.Length;
+			for (i = 0; i < len; i += 3)
+			{
+				v1 = finalVertices[newTris[i]];
+				v2 = finalVertices[newTris[i + 1]];
+				v3 = finalVertices[newTris[i + 2]];
+
+				side1 = v2 - v1;
+				side2 = v3 - v1;
+
+				perp = Vector3.Cross(side1, side2);
+				perp.Normalize ();
+
+				finalUV [newTris [i]]     = new Vector2 (v1.x, v1.z);
+				finalUV [newTris [i + 1]] = new Vector2 (v2.x, v2.z);
+				finalUV [newTris [i + 2]] = new Vector2 (v3.x, v3.z);
+
+				if (perp.x > 0.5f || perp.x < -0.5f) {
+					finalUV [newTris [i]]     = new Vector2 (v1.y, v1.z);
+					finalUV [newTris [i + 1]] = new Vector2 (v2.y, v2.z);
+					finalUV [newTris [i + 2]] = new Vector2 (v3.y, v3.z);
+				}
+				else if (perp.z > 0.5f || perp.z < -0.5f) {
+					finalUV [newTris [i]]     = new Vector2 (v1.x, v1.y);
+					finalUV [newTris [i + 1]] = new Vector2 (v2.x, v2.y);
+					finalUV [newTris [i + 2]] = new Vector2 (v3.x, v3.y);
+				}
+			}
+
+			mesh.uv = finalUV;
+		}
+	}
 }
