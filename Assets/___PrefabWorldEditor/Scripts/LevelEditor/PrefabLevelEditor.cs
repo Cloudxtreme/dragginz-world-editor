003380c71a334405f99bc9004456fc1e032961eb
obrodhage <obrodhage@gmail.com>
2018-05-07 05:47:36 -0700

    more prefab world editor functionality

--- Assets/___PrefabWorldEditor/Scripts/LevelEditor/PrefabLevelEditor.cs
+++ Assets/___PrefabWorldEditor/Scripts/LevelEditor/PrefabLevelEditor.cs
@@ -14,6 +14,7 @@ namespace PrefabWorldEditor
 	public class PrefabLevelEditor : MonoBehaviour
     {
         public Transform playerEdit;
+		public Transform playerPlay;
 
         [SerializeField]
         public Vector3Int levelSize;
@@ -23,10 +24,12 @@ namespace PrefabWorldEditor
 
 		//
 
-		const int CAVE_PATH_A = 0;
-		const int CAVE_PATH_B = 1;
-		const int CAVE_PATH_C1 = 2;
-		const int CAVE_PATH_C2 = 3;
+		const int CAVE_FLOOR = 0;
+		const int CAVE_WALL = 1;
+		const int CAVE_PATH_A = 2;
+		const int CAVE_PATH_B = 3;
+		const int CAVE_PATH_C1 = 4;
+		const int CAVE_PATH_C2 = 5;
 
         private struct Part
         {
@@ -55,21 +58,25 @@ namespace PrefabWorldEditor
         private Vector3 _v3EditPartPos;
 		private int _curRotation;
 
+		private LevelElement _selectedElement;
+
         private float _rayDistance;
         private Ray _ray;
         private RaycastHit _hit;
         private GameObject _goHit;
 
+		private int _playerMode;
+
         private float _timer;
         private float _lastMouseWheelUpdate;
 
-        // ------------------------------------------------------------------------
-        // Use this for initialization
         // ------------------------------------------------------------------------
         void Start()
         {
             _parts = new Dictionary<int, Part>();
 
+			createPart(CAVE_FLOOR, "MDC/CATA_BlockC1", 6f, .75f, 6f);
+			createPart(CAVE_WALL, "MDC/CATA_GenericWall", 3f, 3f, .5f);
 			createPart(CAVE_PATH_A, "MDC/CATA_CavePathA", 5f, 4f, 12f);
 			createPart(CAVE_PATH_B, "MDC/CATA_CavePathB", 5f, 6f, 12f);
 			createPart(CAVE_PATH_C1, "MDC/CATA_CavePathC1", 12f, 3f, 3f);
@@ -82,37 +89,93 @@ namespace PrefabWorldEditor
             createDefaultLevel();
 
 			//_curEditPartId = CAVE_PATH_A;
-			_curEditPart = _parts [CAVE_PATH_A];
+			_curEditPart = _parts [CAVE_FLOOR];
             _v3EditPartPos = Vector3.zero;
 			_goEditPart = createPartAt(_curEditPart.id, -1, -1, -1);
 			_curRotation = 0;
             disableMeshCollider();
+
+			_selectedElement = new LevelElement();
+			_selectedElement.partId = -1;
+
+			_playerMode = 0;
         }
 
-        // Update is called once per frame
+        //
         void Update() {
 
-            _timer = Time.realtimeSinceStartup;
+			_timer = Time.realtimeSinceStartup;
 
-            if (Input.GetKeyDown(KeyCode.P)) {
-                _goEditPart.SetActive(!_goEditPart.activeSelf);
-            }
+			if (Input.GetKeyDown (KeyCode.P)) {
+				setPlayerMode();
+			}
 
-            if (!_goEditPart.activeSelf) {
-                return;
-            }
+			if (_playerMode == 1) {
+				return;
+			}
 
-            _rayDistance = 5f;
-            _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(_ray, out _hit, 100)) {
-                _goHit = _hit.collider.gameObject;
-                _rayDistance = _hit.distance;
-            }
+			if (Input.GetKeyDown (KeyCode.X)) {
+				_goEditPart.SetActive (!_goEditPart.activeSelf);
+			}
 
+			_rayDistance = 5f;
+			_goHit = null;
+			_ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(_ray, out _hit, 100)) {
+				_goHit = _hit.collider.gameObject;
+				_rayDistance = _hit.distance;
+			}
+
+			if (!_goEditPart.activeSelf) {
+				if (Input.GetMouseButtonDown (0)) {
+					if (_goHit != null) {
+						selectElement (_goHit.transform.parent);
+					}
+				}
+				if (_selectedElement.go != null) {
+					positionSelectedElement ();
+				}
+			} else {
+				positionEditPart ();				
+			}
+		}
+
+		//
+		private void setPlayerMode()
+		{
+			_playerMode = (_playerMode == 0 ? 1 : 0);
+
+			playerEdit.gameObject.SetActive (_playerMode == 0);
+			playerPlay.gameObject.SetActive (!playerEdit.gameObject.activeSelf);
+		}
+
+		//
+		private void positionEditPart()
+		{
             _v3EditPartPos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, _rayDistance));
-            _v3EditPartPos.x = Mathf.Clamp(Mathf.RoundToInt(_v3EditPartPos.x / gridSize) * gridSize, 0f, levelSize.x);
-            _v3EditPartPos.y = Mathf.Clamp(Mathf.RoundToInt(_v3EditPartPos.y / gridSize) * gridSize, 0f, levelSize.y);
-            _v3EditPartPos.z = Mathf.Clamp(Mathf.RoundToInt(_v3EditPartPos.z / gridSize) * gridSize, 0f, levelSize.z);
+            _v3EditPartPos.x = Mathf.RoundToInt(_v3EditPartPos.x / gridSize) * gridSize;
+			if (_v3EditPartPos.x - _curEditPart.w / 2 < 0) {
+				_v3EditPartPos.x = _curEditPart.w / 2;
+			}
+			else if (_v3EditPartPos.x + _curEditPart.w / 2 > levelSize.x) {
+				_v3EditPartPos.x = levelSize.x - _curEditPart.w / 2;
+			}
+
+            _v3EditPartPos.y = Mathf.RoundToInt(_v3EditPartPos.y / gridSize) * gridSize;
+			if (_v3EditPartPos.y < 0) {
+				_v3EditPartPos.y = 0;
+			}
+			else if (_v3EditPartPos.y + _curEditPart.h > levelSize.y) {
+				_v3EditPartPos.y = levelSize.y - _curEditPart.h;
+			}
+
+            _v3EditPartPos.z = Mathf.RoundToInt(_v3EditPartPos.z / gridSize) * gridSize;
+			if (_v3EditPartPos.z - _curEditPart.d / 2 < 0) {
+				_v3EditPartPos.z = _curEditPart.d / 2;
+			}
+			else if (_v3EditPartPos.z + _curEditPart.d / 2 > levelSize.z) {
+				_v3EditPartPos.z = levelSize.z - _curEditPart.d / 2;
+			}
             _goEditPart.transform.position = _v3EditPartPos;
 
 			// add wall collision check
@@ -134,17 +197,75 @@ namespace PrefabWorldEditor
                 }
             }
 
-            if (Input.GetMouseButtonDown(0)) {
-                placePart(_goEditPart.transform.position);
-            }
+			if (Input.GetMouseButtonDown (0))
+			{
+				if ((Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) && _curEditPart.id == CAVE_FLOOR) {
+					fillY (_goEditPart.transform.position);
+				}
+				else if ((Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) && _curEditPart.id == CAVE_WALL) {
+					fillZ (_goEditPart.transform.position);
+				}
+				else {
+					placePart (_goEditPart.transform.position);
+				}
+			}
         }
+
+		//
+		private void selectElement(Transform trfmHit)
+		{
+			_selectedElement = new LevelElement();
+			_selectedElement.partId = -1;
+
+			int lenX = (int)((float)levelSize.x / gridSize);
+			int lenY = (int)((float)levelSize.y / gridSize);
+			int lenZ = (int)((float)levelSize.z / gridSize);
+			int x, y, z;
+			for (x = 0; x < lenX; ++x) {
+				for (y = 0; y < lenY; ++y) {
+					for (z = 0; z < lenZ; ++z) {
+						if (_levelElements [x, y, z].go != null && _levelElements [x, y, z].go.transform == trfmHit) {
+							_selectedElement = _levelElements [x, y, z];
+							break;	
+						}
+					}
+				}
+			}
+		}
+
+		//
+		private void positionSelectedElement()
+		{
+			_v3EditPartPos = _selectedElement.go.transform.position;
+
+			if (Input.GetKeyDown (KeyCode.Alpha1)) {
+				_v3EditPartPos.x += gridSize;
+			}
+			else if (Input.GetKeyDown (KeyCode.Alpha2)) {
+				_v3EditPartPos.x -= gridSize;
+			}
+			else if (Input.GetKeyDown (KeyCode.Alpha3)) {
+				_v3EditPartPos.y += gridSize;
+			}
+			else if (Input.GetKeyDown (KeyCode.Alpha4)) {
+				_v3EditPartPos.y -= gridSize;
+			}
+			else if (Input.GetKeyDown (KeyCode.Alpha5)) {
+				_v3EditPartPos.z += gridSize;
+			}
+			else if (Input.GetKeyDown (KeyCode.Alpha6)) {
+				_v3EditPartPos.z -= gridSize;
+			}
+
+			_selectedElement.go.transform.position = _v3EditPartPos;
+		}
 
         //
         private void toggleEditPart() {
 
 			int id = _curEditPart.id;
 			if (++id > CAVE_PATH_C2) {
-				id = CAVE_PATH_A;
+				id = CAVE_FLOOR;
             }
 			_curEditPart = _parts [id];
 
@@ -159,13 +280,13 @@ namespace PrefabWorldEditor
         private void disableMeshCollider () {
 
             GameObject go = _goEditPart;
-            if (go.GetComponent<MeshCollider>()) {
-                go.GetComponent<MeshCollider>().enabled = false;
+            if (go.GetComponent<Collider>()) {
+                go.GetComponent<Collider>().enabled = false;
             }
 			else {
 				foreach (Transform child in go.transform) {
-					if (child.gameObject.GetComponent<MeshCollider> ()) {
-						child.gameObject.GetComponent<MeshCollider> ().enabled = false;
+					if (child.gameObject.GetComponent<Collider> ()) {
+						child.gameObject.GetComponent<Collider> ().enabled = false;
 					}
 				}
 				/*if (go.transform.Find ("part")) {
@@ -183,6 +304,7 @@ namespace PrefabWorldEditor
             int x = (int)(pos.x / gridSize);
             int y = (int)(pos.y / gridSize);
             int z = (int)(pos.z / gridSize);
+			Debug.Log (x+", "+y+", "+z);
 
             LevelElement element = _levelElements[x, y, z];
 
@@ -196,6 +318,36 @@ namespace PrefabWorldEditor
 
             _levelElements[x, y, z] = element;
         }
+
+		//
+		private void fillY(Vector3 pos)
+		{
+			int lenX = (int)((float)levelSize.x / _curEditPart.w);
+			int lenZ = (int)((float)levelSize.z / _curEditPart.d);
+			int x, z;
+			for (x = 0; x < lenX; ++x) {
+				for (z = 0; z < lenZ; ++z) {
+					pos.x = _curEditPart.w / 2 + (x * _curEditPart.w);
+					pos.z = _curEditPart.d / 2 + (z * _curEditPart.d);
+					placePart (pos);
+				}
+			}
+		}
+
+		//
+		private void fillZ(Vector3 pos)
+		{
+			int lenX = (int)((float)levelSize.x / _curEditPart.w);
+			int lenY = (int)((float)levelSize.y / _curEditPart.h);
+			int x, y;
+			for (x = 0; x < lenX; ++x) {
+				for (y = 0; y < lenY; ++y) {
+					pos.x = _curEditPart.w / 2 + (x * _curEditPart.w);
+					pos.y = (y * _curEditPart.h);
+					placePart (pos);
+				}
+			}
+		}
 
         //
         private void createPart(int id, string prefab, float w, float h, float d)
@@ -236,7 +388,7 @@ namespace PrefabWorldEditor
             float h = (float)levelSize.y;
             float d = (float)levelSize.z;
 
-            Vector2 matScale = new Vector2(w / gridSize, h / gridSize);
+            Vector2 matScale = new Vector2(w, h);
             Vector3 goScale = new Vector3(w, h, d);
 
             setWall("wall_f", goScale, new Vector3(w / 2f + 1, h / 2f + 1, d + 1), matScale);
